3b0ecfb06ba6513797ce5dcc85e31ecde78503df
K S Subramanya Char <123807459+s16exe@users.noreply.github.com>
2026-03-10 11:43:24 +0530

    changes

--- sketch-sync-frontend/src/pages/Home.jsx
+++ sketch-sync-frontend/src/pages/Home.jsx
@@ -7,11 +7,11 @@ export default function Home() {
   return (
     <main>
       <button type="button" onClick={() => navigate("/chat/1")}>
-        Chat Group 1
+        Project 1
       </button>
 
       <button type="button" onClick={() => navigate("/chat/2")}>
-        Chat Group 2
+        Project 2
       </button>
     </main>
   );
